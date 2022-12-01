77b53c62a5af75980377b50edad8eddd0d881420
Alex Spencer <aspencer@redboxvoice.com>
2022-12-01 09:35:47 +0000

    Implement day 1 solution

--- AdventOfCode2022/Days/Day1.cs
+++ AdventOfCode2022/Days/Day1.cs
@@ -11,7 +11,31 @@ namespace AdventOfCode2022.Days
 
             IEnumerable<string> data = DataReader.ReadData(this, config);
 
-            return 0;
+
+            List<List<int>> elves = new List<List<int>>();
+
+            List<int> currentElf = new List<int>();
+            foreach(var line in data)
+            {
+                if (line == "\n" || line == "")
+                {
+                    var temp = new List<int>();
+                    temp = currentElf.Select(x => x).ToList();
+                    elves.Add(temp);
+                    currentElf = new List<int>();
+                }
+                else {
+                    currentElf.Add(int.Parse(line));
+                }
+            }
+
+
+            var max = elves.Max(x => x.Sum());
+
+            elves = elves.OrderByDescending(x => x.Sum()).ToList();
+
+            return elves[0].Sum() + elves[1].Sum() + elves[2].Sum();
+
         }
     }
 }
