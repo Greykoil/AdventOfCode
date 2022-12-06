d14b6dcf34c55903cd90b2e0652fd580ca468476
Alex Spencer <aspencer@redboxvoice.com>
2022-12-06 10:21:57 +0000

    Implement Day 6

--- AdventOfCode2022/Days/Day6.cs
+++ AdventOfCode2022/Days/Day6.cs
@@ -6,12 +6,38 @@ namespace AdventOfCode2022.Days
     {
         public long Run(RunConfig config)
         {
-            
+
             Console.WriteLine($"Start running {GetType()}");
 
             IEnumerable<string> data = DataReader.ReadData(this, config);
 
-            return 0;
+            string input = data.First();
+
+            int len = 14;
+
+            for (int i = 0; i < input.Length; ++i)
+            {
+                if (IsStart(input, i, len))
+                {
+                    return i + len;
+                }
+            }
+            throw new Exception("Answer not found");
+        }
+
+        private bool IsStart(string input, int start, int length)
+        {
+            for (int j = 0; j < length; ++j)
+            {
+                for (int k = 1; k < length - j; ++k)
+                {
+                    if (input[start + j] == input[start + j + k])
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
         }
     }
 }
