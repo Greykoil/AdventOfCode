cf923f6355a16388aa9a6027a021da45f1af208a
Alex Spencer <alex.i.j.spencer@gmail.com>
2023-12-01 08:39:23 +0000

    Day 1 solution (#16)
    Co-authored-by: Spencer, Alex <Alex.Spencer@nortechonline.co.uk>

---
+++ AdventOfCode/Days/Y2023/Day1.cs
@@ -0,0 +1,65 @@
+﻿using AdventOfCode.Helper;
+
+namespace AdventOfCode.Days.Y2023
+{
+    internal class Day1 : IDay
+    {
+        public long Run(RunConfig config)
+        {
+            var data = DataReader.ReadData(this, config);
+            int total = 0;
+
+            foreach (var line in data)
+            {
+                for (int i = 0; i < line.Length; ++i)
+                {
+                    var result = IsDigit(line, i);
+                    if (result != -1)
+                    {
+                        total += 10 * result;
+                        break;
+                    }
+                }
+                for (int i = line.Length - 1; i >= 0; --i)
+                {
+                    var result = IsDigit(line, i);
+                    if (result != -1)
+                    {
+                        total += result;
+                        break;
+                    }
+                }
+            }
+            return total;
+        }
+
+        // Return -1 if it isn't a digit
+        private int IsDigit(string line, int position)
+        {
+            if (int.TryParse(line[position].ToString(), out int result))
+            {
+                return result;
+            }
+
+            List<string> numStrings = new List<string>()
+            {
+                "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"
+            };
+
+            foreach (var item in numStrings)
+            {
+                if (position + item.Count() > line.Length)
+                {
+                    continue;
+                }
+                var subStr = line.Substring(position, item.Length);
+                if (subStr == item)
+                {
+                    return numStrings.IndexOf(item) + 1;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
